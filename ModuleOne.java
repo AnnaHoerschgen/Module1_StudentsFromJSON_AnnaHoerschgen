1edb820debfca16862aeccbb52a996d96faf33f3
Anna Hoerschgen <ah0959247@otc.edu>
2025-01-28 12:48:30 -0600

    Building the foundations

---
+++ ModuleOne.java
@@ -0,0 +1,28 @@
+/*
+ * Name: Anna Hoerschgen
+ * Date: January 28, 2025
+ * Filename: ModuleOne.java
+ * Assignment: Students from a [JSON] File
+ */
+
+// Imports
+
+public class ModuleOne {
+    public static void main(String[] args) {
+        // enter main program
+    }
+}
+
+class Student {
+    int id;
+    String name;
+    int age;
+    String grade;
+
+    public Student(int id, String name, int age, String grade) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+}
